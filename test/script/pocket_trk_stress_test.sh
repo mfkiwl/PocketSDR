7c1b12b8517601eb1754407b934ba3f14ebbd567
ttaka <ttaka@yk.rim.or.jp>
2026-07-05 11:21:35 +0900

    CRLF -> LF.

--- test/script/pocket_trk_stress_test.sh
+++ test/script/pocket_trk_stress_test.sh
@@ -5,7 +5,8 @@ cd "$SCRIPT_DIR" || exit 1
 #  pocket_trk real-time test
 #
 #C10="-sig L1CA -prn 194,194,194,194,194 -sig L5I -prn 194,194,194,194,194"
-C10="-sig L1CA -prn 196,196,196,196,196 -sig L5I -prn 196,196,196,196,196"
+#C10="-sig L1CA -prn 196,196,196,196,196 -sig L5I -prn 196,196,196,196,196"
+C10="-sig L1CA -prn 195,195,195,195,195 -sig L5I -prn 195,195,195,195,195"
 
 C50="$C10 $C10 $C10 $C10 $C10" #  50 CH
 C100="$C50 $C50"               # 100 CH
@@ -21,7 +22,7 @@ C700="$C400 $C300"             # 700 CH
 C800="$C400 $C400"             # 800 CH
 C900="$C500 $C400"             # 900 CH
 
-../../bin/pocket_trk $C250 \
+../../bin/pocket_trk $C500 \
 -c ../../conf/pocket_L1L5_24MHz.conf \
--rtcm :10015 -nmea :10016 -log :10017 $@
+-ti 5 -rtcm :10015 -nmea :10016 -log :10017 $@
 
