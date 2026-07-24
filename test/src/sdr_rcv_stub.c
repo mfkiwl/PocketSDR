a823736d9401f8b3f8245e53530a15e8fea11c0a
ttaka <ttaka@yk.rim.or.jp>
2026-07-24 13:33:35 +0900

    Updated.

--- test/src/sdr_rcv_stub.c
+++ test/src/sdr_rcv_stub.c
@@ -12,3 +12,15 @@ void sdr_rcv_array_calib(sdr_rcv_t *rcv, const obsd_t *obs, int nobs,
     (void)nav;
     (void)rr;
 }
+
+int sdr_rcv_write_str(sdr_rcv_t *rcv, int type, uint8_t *data, int size)
+{
+    int bytes = -1;
+
+    for (int i = 0; i < SDR_MAX_STR; i++) {
+        if (!rcv->strs[i] || rcv->str_type[i] != type) continue;
+        int n = sdr_str_write(rcv->strs[i], data, size);
+        if (bytes < 0) bytes = n;
+    }
+    return bytes < 0 ? 0 : bytes;
+}
